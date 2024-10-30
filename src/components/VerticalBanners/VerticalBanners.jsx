16e6baa08b7ab799047c3deec3db2bd5e936e2da
Professor-Dollar <abdulmoid2007@gmail.com>
2024-10-31 01:27:02 +0500

    assignment ready

--- src/components/VerticalBanners/VerticalBanners.jsx
+++ src/components/VerticalBanners/VerticalBanners.jsx
@@ -1,6 +1,6 @@
 import Banner from "./VerticalBanner/Banner";
 import styles from "./VerticalBanners.module.css";
-import { allProducts } from "@/data/productsData"; // Updated to import products directly
+import { allProducts } from "@/data/productsData";
 
 const VerticalBanners = () => {
   const verticalBanners = allProducts.filter(product => allProducts.isVerticalBanner);
